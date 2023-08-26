dd4879018e49f9f4dc35d8324da02578397ebea5
Antonio Padalino <antoniopio.padalino@studenti.unitn.it>
2023-08-26 22:55:53 +0200

    bunch of updates, corrections and stuff. Diffusion working on Mac

--- preprocess all.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# python preprocess_data.py -m "MOT20" --detector
-# python preprocess_data.py -m "MOT20" --submission
-
-python preprocess_data.py -m "MOT17" --detector
-# python preprocess_data.py -m "MOT17" --submission
--- produce_files.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-
-python produce_mot_files.py -o "trackers/exp_detector_20/" -m "best models/edge-predictor_node-model-transformer_edge-model-base_layer-size-250_backbone-efficientnet_v2_l_messages-8_trained_on_MOT20_loss_focal_alpha_0.05_gamma_3.0.pkl" --mot 20
-
-
-
